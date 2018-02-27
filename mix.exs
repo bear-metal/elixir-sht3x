bcb34bcf71d57a0aa4b3690dd072086d2fd7021d
Erkki Eilonen <erkki@itech.ee>
2018-02-27 21:32:09 +0530

    mix hex configs + ex_doc

--- mix.exs
+++ mix.exs
@@ -6,8 +6,12 @@ defmodule SHT3x.MixProject do
       app: :sht3x,
       version: "0.1.0",
       elixir: "~> 1.6",
+      build_embedded: Mix.env == :prod,
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      source_url: "https://github.com/bear-metal/elixir-sht3x"
     ]
   end
 
@@ -22,7 +26,22 @@ defmodule SHT3x.MixProject do
   defp deps do
     [
       {:elixir_ale, "~> 1.0"},
-      {:dialyxir, "~> 0.5", only: [:dev], runtime: false}
+      {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
+      {:ex_doc, ">= 0.0.0", only: :dev}
+    ]
+  end
+
+  defp description() do
+    "Elixir driver for the SHT3x (SHT30, SHT31, SHT35) series temperature and humidity sensors from Sensirion."
+  end
+
+  defp package() do
+    [
+      # These are the default files included in the package
+      files: ["lib", "mix.exs", "README*", "LICENSE"],
+      maintainers: ["Erkki Eilonen"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/bear-metal/elixir-sht3x"}
     ]
   end
 end
